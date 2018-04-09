b7528bd64cbaecd1cfe54ce284252cd147716b1f
Кирилл <belogurov.kirill@mail.ru>
2018-04-09 10:43:59 +0500

    Убраны неиспользуемые параметры из settings.py

--- settings_default.py
+++ settings_default.py
@@ -2,8 +2,6 @@ DEBUG = 1
 DEBUG_DIR = "" # сюда будут писаться результаты выполнения запроса
 
 PROXY = "" # url:port
-PROXY_TYPE = "" # socks5
-PROXY_USERPWD = "" # username:password
 
 trackers = {
     "Rutracker": {
