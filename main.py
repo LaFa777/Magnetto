98d72ec7b110ba56953620857cfd72afff60c114
LaFa777 <belogurov.kirill@mail.ru>
2018-04-14 17:40:48 +0500

    обновлен example файл

--- main.py
+++ main.py
@@ -1,57 +1,31 @@
-import settings
-import sys
-import trackers
-import inspect
 from grab import Grab
-from trackers.core import Category
-
-def init_apis():
-    """
-    Инициализирует апишки к трекерам
-    """
-    apis = {}
-    for name, obj in inspect.getmembers(sys.modules["trackers.apis"]):
-        if inspect.isclass(obj):
-            # инициализируем grab объект для каждого трекера
-            grab = Grab()
-            # TODO: при отсутствии свойства в массиве env вылазит ошибка (заменить на settings.py)
-            # Сохранение последней загруженной страницы
-            if settings.DEBUG and settings.DEBUG_DIR:
-                grab.setup(log_file="{dir}/{class_name}.html".format(
-                    dir=settings.DEBUG_DIR,
-                    class_name=name
-                ))
-            # директория для хранения кук
-            if settings.trackers[name]["cookies_dir"]:
-                grab.setup(cookiefile="{dir}/{class_name}.cookie".format(
-                    dir=settings.trackers[name]["cookies_dir"],
-                    class_name=name
-                ))
-            # настройка прокси сервера
-            grab.setup(proxy=settings.PROXY)
-
-            # логин и пароль читаем из аргументов
-            login = settings.trackers[name]["login"]
-            password = settings.trackers[name]["password"]
-            if(not login or not password):
-                print("Api " + name +
-                      " не инициализировано (не установлены логин или пароль)")
-            apis[name] = obj(grab, login, password)
-
-            # попытаем авторизоваться
-            try:
-                apis[name].authorization()
-            except trackers.core.errors.TrackersCaptchaError as err:
-                print(err)
-                str = input()
-                apis[name].authorization(captcha_str=str)
-    return apis
+import settings
+from magnetto import ApiDispatcher, Category, MagnettoCaptchaError
 
 
 def main():
-    apis = init_apis()
-    # проверка работы
-    print(vars(apis["Rutracker"].search("Начало", categories=[Category.Film], limit=1)[0]))
+    # настраиваем граб объект с прокси
+    grab = Grab()
+    grab.setup(proxy=settings.PROXY, proxy_userpwd=settings.PROXY_USERPWD,
+               proxy_type=settings.PROXY_TYPE)
+
+    dp = ApiDispatcher(grab=grab, log_dir=settings.LOG_DIR,
+                       coockie_dir=settings.COOCKIE_DIR)
+
+    # проинициализируем каждый трекер из настроек
+    for api, conf in settings.trackers.items():
+        try:
+            dp.authorization(api, conf["login"], conf["password"])
+        except MagnettoCaptchaError as err:
+            print("Woop woop! Captcha is here: " + err.url)
+            print('Please input catpcha: ', end='')
+            captcha = input()
+            # попробуем снова...
+            dp.authorization(api, conf["login"], conf["password"], captcha)
+
+    # проверим работоспособносить поиска
+    print(dp.search("приключения капитана врунгеля",
+                    categories=[Category.FILMS], limit=1))
 
 
 if __name__ == '__main__':
