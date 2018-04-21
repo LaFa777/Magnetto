eb252da5d6955f88d0d046a6c545622c752c303f
LaFa777 <belogurov.kirill@gmail.com>
2018-04-21 20:45:56 +0500

    Полноценное определение категории для RutrackerParser

--- magnetto/parsers/rutracker_parser.py
+++ magnetto/parsers/rutracker_parser.py
@@ -1,5 +1,6 @@
 import magnetto
-from magnetto import (BaseParser, transformParseError, ResultParseSearchPage)
+from magnetto import (BaseParser, transformParseError, ResultParseSearchPage,
+                      Category)
 
 
 class RutrackerParser(BaseParser):
@@ -9,17 +10,60 @@ class RutrackerParser(BaseParser):
     def __init__(self):
         self.HOME = magnetto.RUTRACKER_URL
 
+        # получаем индекс нашего элемента в дереве
+    def parse_category(self, doc):
+        """Возвращает элемент Category, соответствующий данному элементу
+        поиска
+        """
+        category = {"root": "", "root_forum": "", "forum": ""}
+
+        # заполняем форум из поиска
+        category["forum"] = doc.xpath('td[3]/div/a/text()')[0]
+
+        # заполняем корневой форум
+        el = doc.xpath('//option[contains(text(), "{}")]'
+                       .format(category["forum"]))[0]
+        if "root_forum" in el.classes:
+            category["root_forum"] = el.text
+        else:
+            while True:
+                el = el.getprevious()
+                if el is None:
+                    break
+                if "root_forum" in el.classes:
+                    category["root_forum"] = el.text
+                    break
+
+        # заполняем категорию
+        category["root"] = el.getparent().get("label")
+
+        # Определяем конечную категорию
+        if "Мульфильмы" in category["root_forum"] or \
+            "Мультсериалы" in category["root_forum"] or \
+                "Аниме" in category["root_forum"]:
+            return Category.CARTOONS
+        elif "кино" in category["root_forum"].lower() or \
+                "Video" in category["root_forum"]:
+            return Category.FILMS
+        elif "сериалы" in category["root_forum"].lower():
+            return Category.TV_SERIES
+        # TODO: добавить остальные
+        else:
+            return Category.UNDEFINED
+
     @transformParseError
     def parse_search(self, doc):
         result_items = []
         for tr in doc.tree.xpath('//table[@id="tor-tbl"]/tbody/tr'):
             topic_id = tr.xpath('td[4]/div/a/@data-topic_id')[0]
+            size_int = int(tr.xpath('td[6]/u/text()')[0])
+            category = self.parse_category(tr)
             item = ResultParseSearchPage(
                 id=topic_id,
-                category=tr.xpath('td[3]/div/a/text()')[0],
+                category=category,
                 name=tr.xpath('td[4]/div/a/text()')[0],
                 url=self.HOME + "viewtopic.php?t=" + str(topic_id),
-                size=tr.xpath('td[6]/u/text()')[0],
+                size=str(int(size_int/1024/1024)),
                 seeders=tr.xpath('td[7]/u/text()')[0],
                 leechers=tr.xpath('td[8]/b/text()')[0],
                 downloads=tr.xpath('td[9]/text()')[0],
@@ -28,7 +72,7 @@ class RutrackerParser(BaseParser):
                 magnet=""  # TODO:
             )
             result_items.append(item)
-        return tuple(result_items)
+        return result_items
 
     @transformParseError
     def parse_topic(self, doc):
