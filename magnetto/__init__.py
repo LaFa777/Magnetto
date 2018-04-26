5a7abb9c56eb40107ab38456dc80f776db6c979b
Кирилл <belogurov.kirill@mail.ru>
2018-04-26 13:02:22 +0500

    Изменен порядок импортирования модуля filter_handlers

--- magnetto/__init__.py
+++ magnetto/__init__.py
@@ -15,7 +15,10 @@ from .parsers.rutracker_parser import RutrackerParser
 from .parsers.kinozal_parser import KinozalParser
 
 from .apis.core import GlobalFilters, api_filters_method
-from .apis import filter_handlers
+from .apis.filter_handlers import (handler_filter_size, handler_filter_nozeroseeders,
+                                   handler_filter_category, handler_filter_nowords,
+                                   handler_filter_registred,
+                                   handler_filter_noequalsize)
 from .apis.mixins import CheckAuthMixin, LastRequestMixin
 from .apis.base_api import BaseApi
 from .apis.rutracker_api import RutrackerApi
@@ -33,6 +36,9 @@ __all__ = [
     'ResultParse', 'transformParseError', 'parse_date', 'parse_size',
     'BaseParser', 'RutrackerParser', 'KinozalParser',
     'GlobalFilters', 'api_filters_method',
+    'handler_filter_size', 'handler_filter_nozeroseeders',
+    'handler_filter_category', 'handler_filter_nowords',
+    'handler_filter_registred', 'handler_filter_noequalsize',
     'CheckAuthMixin', 'LastRequestMixin',
     'BaseApi', 'RutrackerApi', 'KinozalApi',
     'ApiDispatcher'
--- magnetto/apis/__init__.py
+++ magnetto/apis/__init__.py
@@ -1,5 +1,8 @@
 from .core import GlobalFilters, api_filters_method
-from . import filter_handlers
+from .filter_handlers import (handler_filter_size, handler_filter_nozeroseeders,
+                              handler_filter_category, handler_filter_nowords,
+                              handler_filter_registred,
+                              handler_filter_noequalsize)
 from .mixins import LastRequestMixin, CheckAuthMixin
 from .base_api import BaseApi
 from .rutracker_api import RutrackerApi
@@ -7,6 +10,9 @@ from .kinozal_api import KinozalApi
 
 __all__ = (
     'GlobalFilters', 'api_filters_method',
+    'handler_filter_size', 'handler_filter_nozeroseeders',
+    'handler_filter_category', 'handler_filter_nowords',
+    'handler_filter_registred', 'handler_filter_noequalsize',
     'LastRequestMixin', 'CheckAuthMixin',
     'BaseApi', 'RutrackerApi', 'KinozalApi',
 )
